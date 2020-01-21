f8ef391e7a4e4f6adc4e407b1925b7a55cb2c886
Mike38 <mshar50055@gmail.com>
2020-01-21 23:56:18 +0300

    koa.js app work has been begun

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const controllerIndex = require("./controller/index")
+const controllerLogin = require("./controller/login")
+const controllerAdmin = require("./controller/admin")
+
+const router = express.Router()
+
+router.get("/", controllerIndex.getIndex)
+router.post("/", controllerIndex.postFormData)
+router.get("/login", controllerLogin.getLogin)
+router.post("/login", controllerLogin.auth)
+router.get("/admin", controllerAdmin.getAdmin)
+router.post("/admin/skills/", controllerAdmin.updateSkills)
+router.get("/admin/upload", controllerAdmin.upload)
+
+module.exports = router
--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,35 @@
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
+const flash = require("connect-flash")
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const fs = require("fs")
+const config = require("./config")
 
-import { square, MyClass } from './module';
+const server = express()
+server.set("source", path.join(_dirname, config.source))
+server.set("source engine", "pug")
 
-console.log(square(5));
-var cred = {
-  name: 'Юрий Кучма',
-  enrollmentNo: 11115078
-};
-var x = new MyClass(cred);
-console.log(x.getName());
+server.use(express.static(path.join(process.cwd(), config.public)))
+
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+
+server.use(cookieParser())
+server.use(session({
+  resave: true,
+  saveUninitialised: true,
+  secret: "login",
+  cookie: {
+    maxAge: 60000
+  }
+}))
+server.use(session({
+  secret: "mail",
+  saveUninitialised: true,
+  resave: true
+}))
+
+server.use(flash())
+server.use("/", require("./routes/index"))
